3353387d823bd8d198ccbafb9150ee9b70fb3b37
Steven Zeiler <zeiler.steven@gmail.com>
2015-05-05 19:37:13 -0700

    [FIX] CREATE not INDEX in callbacks controller

--- controllers/callbacks.js
+++ controllers/callbacks.js
@@ -2,7 +2,7 @@
 module.exports = function(models, lib) {
 
   return {
-    index: function(req, res, next) {
+    create: function(req, res, next) {
       console.log('callback received', req.body)
       res.status(200).send({
         success: true
